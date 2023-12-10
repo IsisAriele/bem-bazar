608e84d6b8c83706da5b8257e8009ebb450a4851
IsisAriele <isisariele1@gmail.com>
2023-12-10 20:20:34 -0300

    Finalizando models da aplicação

--- mysite/bazar/admin.py
+++ mysite/bazar/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Usuario, Item, Evento, Reserva
+from .models import Item, Evento, Reserva
 
-admin.site.register(Usuario)
 admin.site.register(Item)
 admin.site.register(Evento)
 admin.site.register(Reserva)
--- mysite/bazar/models.py
+++ mysite/bazar/models.py
@@ -1,40 +1,30 @@
 from django.db import models
-# from django.utils import timezone
-# import datetime
-
-class Usuario(models.Model):
-    nome = models.CharField(max_length=200)
-    email = models.EmailField(unique=True)
-    senha = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=20, blank=True, null=True)
-
-    def __str__(self):
-        return self.nome
+from django.contrib.auth.models import User
  
 class Evento(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     ativo = models.BooleanField(default=True)
-    nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=200)
-    data_inicio = models.DateTimeField('Data do início do evento')
-    data_fim = models.DateTimeField('Data do fim do evento')
+    nome = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    data_inicio = models.DateTimeField()
+    data_fim = models.DateTimeField()
     poster = models.ImageField(upload_to='posters/')
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome} ----- {self.usuario.username}"
 
 class Item(models.Model):
     evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
     foto = models.ImageField(upload_to='itens/')
-    descricao = models.CharField(max_length=200)
-    preco = models.FloatField()
+    nome = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return self.descricao
+        return f"{self.nome} ----- {self.evento.nome}"
 
 class Reserva(models.Model):
-    data = models.DateTimeField('Data da reserva')
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.OneToOneField(Item, on_delete=models.CASCADE)
-    # item = models.ForeignKey(Item,  unique=True, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return f"{self.usuario.username} ----- {self.item.nome}"
